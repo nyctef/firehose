bee367dea01d13c85a389a98d8fbf3b449f7cd70
Mark Jordan <nyctef@nyctef.com>
2015-04-06 04:41:34 +0100

    add minimal route for index

--- website/website_app.py
+++ website/website_app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import config
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
+
+# set up jade
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+@app.route('/')
+def show_index():
+    return render_template('index.jade')
 
 if __name__ == '__main__':
     app.run()
