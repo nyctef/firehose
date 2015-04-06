86981a178786541864f89aa13e3b74b30ecabe32
Mark Jordan <nyctef@nyctef.com>
2015-04-06 19:20:41 +0100

    add extra line of test data

--- scripts/20_data.sql
+++ scripts/20_data.sql
@@ -7,4 +7,5 @@ values
 ('irc', 'bnc.foo.net', '{"server":"bnc.foo.net", "channels":["general"]}');
 
 select add_message(1, '#general', 'someguy', 'ping', 'hey, what you doin', 'plain');
+select add_message(1, '#general', 'a.n.other', 'normal', 'this is a message', 'plain');
 
